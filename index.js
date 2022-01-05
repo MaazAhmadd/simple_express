ff6a864c980dddcd28e6583fdd69418923ed4497
Maaz Ahmad <maaz.ahmad213@gmail.com>
2022-01-05 12:10:28 +0500

    update

--- index.js
+++ index.js
@@ -2,36 +2,31 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const axios = require("axios");
-// app.use((req, res, next) => {
-//   const user_id = req.query.user_id;
-//   if (!user_id) {
-//     res.redirect("https://t.me/SaylorBNB_Bot");
-//   }
-//   // next();
-// });
-app.use("/game", express.static("game"));
+
+app.use("/game", express.static("game", { index: false }));
 
 // (req, res, next) => {
 //   const user_id = req.query.user_id;
 //   console.log(!user_id);
-//   if (user_id) {
-//     next();
+//   if (!user_id) {
+//     res.redirect("https://t.me/SaylorBNB_Bot");
 //   }
-//   res.redirect("https://t.me/SaylorBNB_Bot");
+//   next();
 // },
 app.get("/savegame", (req, res) => {
   const score = req.query.score;
   const user_id = req.query.user_id;
-  console.log("score : ", score, " user : ", user_id);
+  const message_id = req.query.message_id;
+  const chat_id = req.query.chat.id;
+
   if (!user_id) {
     res.redirect("https://t.me/SaylorBNB_Bot");
   }
   if (score) {
     axios
-      .post(`https://api.telegram.org/bot${process.env.BOT}/setGameHighScore`, {
-        user_id,
-        score,
-      })
+      .get(
+        `https://api.telegram.org/bot${process.env.BOT}/setGameHighScore?score=${score}&user_id=${user_id}&message_id${message_id}&chat_id=${chat_id}`
+      )
       .then((r) => {
         res.send(r.data);
       });
