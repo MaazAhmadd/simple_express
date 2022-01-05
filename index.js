db655915ca4ec681fdc4ddef472f9eeb9da7d152
Maaz Ahmad <maaz.ahmad213@gmail.com>
2022-01-05 11:53:21 +0500

    s

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const axios = require("axios");
+// app.use((req, res, next) => {
+//   const user_id = req.query.user_id;
+//   if (!user_id) {
+//     res.redirect("https://t.me/SaylorBNB_Bot");
+//   }
+//   // next();
+// });
 app.use("/game", express.static("game"));
 
+// (req, res, next) => {
+//   const user_id = req.query.user_id;
+//   console.log(!user_id);
+//   if (user_id) {
+//     next();
+//   }
+//   res.redirect("https://t.me/SaylorBNB_Bot");
+// },
 app.get("/savegame", (req, res) => {
   const score = req.query.score;
   const user_id = req.query.user_id;
@@ -13,7 +28,7 @@ app.get("/savegame", (req, res) => {
   }
   if (score) {
     axios
-      .post(`https://api.telegram.org/${process.env.BOT}/setGameHighScore`, {
+      .post(`https://api.telegram.org/bot${process.env.BOT}/setGameHighScore`, {
         user_id,
         score,
       })
